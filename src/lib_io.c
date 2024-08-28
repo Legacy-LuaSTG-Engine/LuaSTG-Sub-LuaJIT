b288fd668cce84cf9cf1da4a90b3db508ebf0cbd
璀境石 <git@chuikingshek.com>
2024-08-28 10:09:13 +0800

    utf-8 io

--- src/lib_io.c
+++ src/lib_io.c
@@ -26,6 +26,10 @@
 #include "lj_ff.h"
 #include "lj_lib.h"
 
+#ifdef _WIN32
+#include "lj_win32.h"
+#endif
+
 /* Userdata payload for I/O file. */
 typedef struct IOFileUD {
   FILE *fp;		/* File handle. */
@@ -84,7 +88,11 @@ static IOFileUD *io_file_open(lua_State *L, const char *mode)
 {
   const char *fname = strdata(lj_lib_checkstr(L, 1));
   IOFileUD *iof = io_file_new(L);
+#ifdef _WIN32
+  iof->fp = _u8fopen(fname, mode);
+#else
   iof->fp = fopen(fname, mode);
+#endif
   if (iof->fp == NULL)
     luaL_argerror(L, 1, lj_strfmt_pushf(L, "%s: %s", fname, strerror(errno)));
   return iof;
@@ -412,7 +420,11 @@ LJLIB_CF(io_open)
   GCstr *s = lj_lib_optstr(L, 2);
   const char *mode = s ? strdata(s) : "r";
   IOFileUD *iof = io_file_new(L);
+#ifdef _WIN32
+  iof->fp = _u8fopen(fname, mode);
+#else
   iof->fp = fopen(fname, mode);
+#endif
   return iof->fp != NULL ? 1 : luaL_fileresult(L, 0, fname);
 }
 
@@ -428,7 +440,11 @@ LJLIB_CF(io_popen)
   fflush(NULL);
   iof->fp = popen(fname, mode);
 #else
+#ifdef _WIN32
+  iof->fp = _u8popen(fname, mode);
+#else
   iof->fp = _popen(fname, mode);
+#endif
 #endif
   return iof->fp != NULL ? 1 : luaL_fileresult(L, 0, fname);
 #else

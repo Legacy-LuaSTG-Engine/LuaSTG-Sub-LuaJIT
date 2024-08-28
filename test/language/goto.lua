b1f072bf6f1a0d994785856d4e1aeb3867b2d382
璀境石 <git@chuikingshek.com>
2024-08-28 11:15:18 +0800

    better test

--- test/language/goto.lua
+++ test/language/goto.lua
@@ -1,6 +1,24 @@
-local TRUE = true
-if TRUE then
-    goto END
-end
-error("skip me")
-::END::
+local test = require("test")
+local try = require("try")
+
+test.add("disable goto", function()
+    local result = true
+    try({
+        function()
+            require("language.broken.goto")
+            result = false
+        end,
+    })
+    return result
+end)
+
+test.add("disable goto label", function()
+    local result = true
+    try({
+        function()
+            require("language.broken.goto_label")
+            result = false
+        end,
+    })
+    return result
+end)
